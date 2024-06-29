635f146783f211b3a3acbf0de148b0f34d21cdd6
Ana Maria Mtz <100534348+MtzAna26@users.noreply.github.com>
2024-06-29 12:14:16 -0600

    Cambios

--- templates/ebazar/index.php
+++ templates/ebazar/index.php
@@ -7,7 +7,7 @@
   <meta name="description" content="Ebazar HTML5 Template">
   <title>TESOROS ARTESANALES</title>
   <!-- Favicon -->
-  <link rel="shortcut icon" type="image/x-icon" href="assets/media/tesoroslogo.jpg">
+  <link rel="shortcut icon" type="image/x-icon" href="/templates/ebazar/assets/media/tesoroslogo.jpg">
   <!-- All CSS files -->
   <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/vendor/font-awesome.css">
@@ -28,15 +28,16 @@
   <?php require('footer.php'); ?>
 
   <?php
-    if ($_REQUEST["view"] == "")
-        echo '<script type="text/javascript">window.location.replace("./inicio");</scrip>';
-    else if (file_exists("modules/".$_REQUEST["view"].".php"))
+    if ($_REQUEST["view"] == "") {
+        echo '<script type="text/javascript">window.location.replace("./inicio");</script>';
+    } else if (file_exists("modules/".$_REQUEST["view"].".php")) {
         include_once "modules/".$_REQUEST["view"].".php";
-    else
-        echo '<script type="type/javascript">window.location.replace("./inicio);>script>';
+    } else {
+        echo '<script type="text/javascript">window.location.replace("./inicio");</script>';
+    }
   ?>
 
-  <!-- JavaScript -->
+  <!-- JavaScript  -->
   <script src="assets/js/vendor/jquery.min.js"></script>
   <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
   <script src="assets/js/vendor/slick.min.js"></script>
